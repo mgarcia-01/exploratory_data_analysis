3ba70dbfea9f7c35a7fca47cf9675ae75b14d200
Michael Garcia <mgarcia4703@gmail.com>
2017-06-25 03:42:51 -0700

    updates

--- Project Week 4.R
+++ Project Week 4.R
@@ -8,8 +8,7 @@ SCC <- readRDS(SCCFile)
 
 #NEI <- NEI[which(NEI$year >= 1999 & NEI$year <= 2008),]
 NEISCC <- merge(x = NEI, y = SCC, by = "SCC", all.y = TRUE)
-NEISCC1 <- NEISCC(which(NEISCC$fips == "24510"))
-
+NEISCC1 <- NEISCC[ which(NEISCC$fips == "24510"), ]
 #plot 1
 yearEmission <- aggregate(NEISCC$Emissions, list(NEISCC$year), sum)
 names(yearEmission) <- c("year","Emissions")
@@ -22,3 +21,42 @@ png(filename = plot1img,
 plot1 <- barplot(yearEmission$Emissions, names.arg = yearEmission$year)
 dev.off()
   #plot(yearEmission$year, yearEmission$Emissions, xlab = "year", ylab = "emissions", type = "b")
+
+
+###############    #plot 2  ###############
+plot2img <- file.path(getwd(), paste("plot2", ".png",sep = ""))
+yearEmission2 <- aggregate(NEISCC1$Emissions, list(NEISCC1$year), sum)
+names(yearEmission2) <- c("year","Emissions")
+
+png(filename = plot2img,
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white",  res = NA,## ...,
+    #type = c("cairo", "cairo-png", "Xlib", "quartz"), 
+    antialias = c("default"))
+
+plot2 <- plot(yearEmission2$year, yearEmission2$Emissions, xlab = "year", ylab = "emissions", type = "b")
+
+dev.off()
+
+
+###############    #plot 3  ###############
+plot3img <- file.path(getwd(), paste("plot3", ".png",sep = ""))
+yearEmission2 <- aggregate(NEISCC1$Emissions, list(NEISCC1$year), sum)
+names(yearEmission2) <- c("year","Emissions")
+
+png(filename = plot3img,
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white",  res = NA,## ...,
+    #type = c("cairo", "cairo-png", "Xlib", "quartz"), 
+    antialias = c("default"))
+
+plot2 <- plot(yearEmission2$year, yearEmission2$Emissions, xlab = "year", ylab = "emissions", type = "b")
+
+dev.off()
+
+
+
+
+
+
+
--- plot1.R
+++ plot1.R
@@ -1,47 +1,24 @@
-library(datasets)
-############################## Plot 1 #########################
+#source(file.path(getwd(), paste("plot4", ".R",sep = "")))
 
+NEIFile <- file.path(getwd(), paste("summarySCC_PM25", ".rds",sep = ""))
+SCCFile <- file.path(getwd(), paste("Source_Classification_Code", ".rds",sep = ""))
+plot1img <- file.path(getwd(), paste("plot1", ".png",sep = ""))
+NEI <- readRDS(NEIFile)
+SCC <- readRDS(SCCFile)
 
-fileURL <- file.path(getwd(), paste("household_power_consumption", ".txt",sep = ""))
-plot1URL <- file.path(getwd(), paste("plot1", ".png",sep = ""))
+#NEI <- NEI[which(NEI$year >= 1999 & NEI$year <= 2008),]
+NEISCC <- merge(x = NEI, y = SCC, by = "SCC", all.y = TRUE)
+NEISCC1 <- NEISCC(which(NEISCC$fips == "24510"))
 
-
-
-
-
-#source(file.path(getwd(), paste("reqFileURLS", ".R",sep = "")))
-
-
-hpc <- read.table(file = fileURL, header = TRUE,sep = ";")
-
-hpc$Global_active_power <- as.numeric(as.character(hpc$Global_active_power))
-myTime <-strptime(paste(hpc$Date, hpc$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
-hpc <- cbind(myTime,hpc)
-hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
-hpc <-  hpc[ which(hpc$Date == "2007-2-1"
-                   | hpc$Date == "2007-2-2"), ]
-
-## plot1.png ,  plot1.R  ####
-
-#hist(hpc$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-## this prints to .png file
-png(filename = plot1URL,
+#plot 1
+yearEmission <- aggregate(NEISCC$Emissions, list(NEISCC$year), sum)
+names(yearEmission) <- c("year","Emissions")
+png(filename = plot1img,
     width = 480, height = 480, units = "px", pointsize = 12,
     bg = "white",  res = NA,## ...,
     #type = c("cairo", "cairo-png", "Xlib", "quartz"), 
     antialias = c("default"))
 
-hist(hpc$Global_active_power, col = "red"   
-     #, breaks=seq(0,6, by = 1)
-     #, freq = TRUE  ,xaxt='n' 
-     # ,labels = seq(0,6,by=1)
-     ,main="Global Active Power"
-     #, ylim = c(0,1200)
-     ,xlab="Global Active Power (kilowatts)"
-)
-
+plot1 <- barplot(yearEmission$Emissions, names.arg = yearEmission$year)
 dev.off()
-
-## not needed
-#axis(side=1, at=seq(0,6, by=1), labels=seq(0,6, by=1))
-#axis(side=2, at=seq(0,1000,200), labels=seq(0,1000,200))
+#plot(yearEmission$year, yearEmission$Emissions, xlab = "year", ylab = "emissions", type = "b")
